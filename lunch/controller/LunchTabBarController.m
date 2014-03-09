dd0cf1dca9d358e0ad12d602fa600b3cb1e4a11f
u1 <u1.kobayashi@gmail.com>
2014-03-09 22:44:43 +0900

    iOS6に対応

--- lunch/controller/LunchTabBarController.m
+++ lunch/controller/LunchTabBarController.m
@@ -40,9 +40,17 @@ int const kPickerViewTag = 1;
                              @"13:00", @"13:30", @"14:00", @"14:30",
                              @"15:00", @"15:30", @"16:00"];
     
+    
+    if (floor(NSFoundationVersionNumber) > NSFoundationVersionNumber_iOS_6_1) {
+        self.edgesForExtendedLayout = UIRectEdgeNone;
+    }
+    
     // 広告
-    self.nadView = [[NADView alloc] initWithFrame:CGRectMake(0, self.view.frame.size.height - NAD_ADVIEW_SIZE_320x50.height,
-                                                             NAD_ADVIEW_SIZE_320x50.width, NAD_ADVIEW_SIZE_320x50.height)];
+    int contentHeight = self.view.frame.size.height -
+    self.navigationController.navigationBar.frame.size.height - 20; // screen - navigationBar - statusBar
+    self.nadView = [[NADView alloc] initWithFrame:
+                    CGRectMake(0, contentHeight - NAD_ADVIEW_SIZE_320x50.height,
+                               NAD_ADVIEW_SIZE_320x50.width, NAD_ADVIEW_SIZE_320x50.height)];
     [self.nadView setNendID:[[[NSBundle mainBundle] infoDictionary] objectForKey:kNendId]
                      spotID:[[[NSBundle mainBundle] infoDictionary] objectForKey:kNendSpotId]];
     [self.nadView setDelegate:self];
